3596e0a073abb107a0f18ccbd1e867b1a0129acf
Brent Monning <brent.monning.jr@gmail.com>
2017-10-04 18:44:22 -0700

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,9 @@
+/* Open */
+function openMenu() {
+    document.getElementById("menu").style.height = "100%";
+}
+
+/* Close */
+function closeMenu() {
+    document.getElementById("menu").style.height = "0%";
+} 
