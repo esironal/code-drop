656be1ca24e432e31a02f5ad53dcbbc49e05c48f
Brent Monning <brent.monning.jr@gmail.com>
2017-10-04 18:57:48 -0700

    Delete menu.js

--- menu.js
+++
@@ -1,9 +0,0 @@
-/* Open */
-function openMenu() {
-    document.getElementById("menu").style.height = "100%";
-}
-
-/* Close */
-function closeMenu() {
-    document.getElementById("menu").style.height = "0%";
-} 
