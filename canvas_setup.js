a7f5882dd339562d698094215bcc1d1b1821eae4
Brent Monning <brent.monning.jr@gmail.com>
2017-11-23 13:10:11 -0800

    Update canvas_setup.js

--- canvas_setup.js
+++ canvas_setup.js
@@ -1,5 +1,8 @@
 // Declares the setup function.
 function setup() {
   // Creates the canvas.
-  createCanvas(400, 400);
+  var canvas = createCanvas(400, 400);
+  
+  // Moves the canvas to the canvas code.
+  canvas.parent('canvas_div');
 }
